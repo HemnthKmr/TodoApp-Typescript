5ba9b10f372d37fac9a6c1fc98a0b235b3a9a9b9
G <hemanthkumar.g@hp.com>
2021-11-22 11:46:55 +0000

    Added logic for TodoApp

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,39 @@
 "use strict";
-console.log("Hello");
+const inputBox = document.querySelector(".inputSection input");
+const addBtn = document.querySelector(".inputSection button");
+const todoList = document.querySelector(".TodoList");
+let listArr;
+inputBox.onkeyup = () => {
+    let userData = inputBox.value;
+    (userData.trim()) ? addBtn.disabled = false : addBtn.disabled = true;
+};
+addBtn.onclick = () => {
+    let userData = inputBox.value;
+    let getLocalStorage = localStorage.getItem("Todo");
+    if (getLocalStorage == null) {
+        listArr = [];
+    }
+    else {
+        listArr = JSON.parse(getLocalStorage);
+    }
+    listArr.push(userData);
+    localStorage.setItem("Todo", JSON.stringify(listArr));
+    showTasks();
+};
+const showTasks = () => {
+    let getLocalStorage = localStorage.getItem("Todo");
+    if (getLocalStorage == null) {
+        listArr = [];
+    }
+    else {
+        listArr = JSON.parse(getLocalStorage);
+    }
+    let newLiTag = '';
+    listArr.forEach((element, index) => {
+        newLiTag = `<p>${element}  <button onclick=deleteTask(${index})>Delete</button></p>`;
+    });
+    todoList.innerHTML += newLiTag;
+    inputBox.value = "";
+};
+showTasks();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,42 @@
-console.log("Hello");
+const inputBox = <HTMLInputElement>document.querySelector(".inputSection input")!;
+const addBtn = <HTMLButtonElement>document.querySelector(".inputSection button");
+const todoList = <HTMLButtonElement>document.querySelector(".TodoList");
+let listArr: string[];
+
+inputBox.onkeyup = () => {
+    let userData = inputBox.value;
+    (userData.trim()) ? addBtn.disabled = false :  addBtn.disabled = true;
+}
+
+addBtn.onclick = () => {
+    let userData = inputBox.value;
+    let getLocalStorage = localStorage.getItem("Todo");
+    if(getLocalStorage == null){
+        listArr = [];
+    }
+    else{
+        listArr = JSON.parse(getLocalStorage)
+    }
+    listArr.push(userData);
+    localStorage.setItem("Todo",JSON.stringify(listArr));
+    showTasks();
+}
+
+const showTasks = () => {
+    let getLocalStorage = localStorage.getItem("Todo");
+    
+    if(getLocalStorage == null){
+        listArr = [];
+    }
+    else{
+        listArr = JSON.parse(getLocalStorage)
+    }
+    let newLiTag = '';
+    listArr.forEach((element,index) => {
+       newLiTag = `<p>${element}  <button onclick=deleteTask(${index})>Delete</button></p>`
+    });
+    todoList.innerHTML += newLiTag;
+    inputBox.value="";
+}
+
+showTasks();
